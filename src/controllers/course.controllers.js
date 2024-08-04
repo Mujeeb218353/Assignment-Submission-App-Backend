2ed3acf656cc5982a815834ab49424a16bc3678d
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-08-04 16:40:49 +0500

    Created editCourse, deleteCourse, deleteCourseCampus, and deleteCourseCity controllers and their routes

--- src/controllers/course.controllers.js
+++ src/controllers/course.controllers.js
@@ -124,55 +124,83 @@ import { Admin } from "../models/admin.model.js";
 // });
 
 const addCourse = asyncHandler(async (req, res) => {
-    const { name, cityId, campusId, userId } = req.body;
   
-    console.log(name, cityId, campusId, userId);
-  
-    if (!name) throw new apiError(400, "Course name is required");
-    if (!cityId) throw new apiError(400, "City is required");
-    if (!campusId) throw new apiError(400, "Campus is required");
-    if (!userId) throw new apiError(400, "User is required");
-  
-    const user = await Admin.findById(req.admin._id);
-    const city = await City.findById(cityId);
-    const campus = await Campus.findById(campusId);
-  
-    if (!city) throw new apiError(404, "City not found");
-    if (!campus) throw new apiError(404, "Campus not found");
-    if (!user) throw new apiError(404, "Unauthorized user");
-  
-    const existedCourse = await Course.findOne({ name });
-  
-    if (existedCourse) {
-      const existedCity = Array.isArray(existedCourse.city) && existedCourse.city.includes(cityId);
-      const existedCampus = Array.isArray(existedCourse.campus) && existedCourse.campus.includes(campusId);
-  
-      if (existedCity && existedCampus) {
-        throw new apiError(400, "Course already exists");
-      }
-  
-      if (existedCity && !existedCampus) {
-        existedCourse.campus.push(campusId);
-        await existedCourse.save();
-      }
-  
-      if (!existedCity) {
-        existedCourse.city.push(cityId);
-        existedCourse.campus.push(campusId);
-        await existedCourse.save();
-      }
-    } else {
-      const newCourse = new Course({
-        name,
-        city: [cityId],
-        campus: [campusId],
-        createdBy: userId,
-      });
-  
-      await newCourse.save();
+  const { name, cityId, campusId } = req.body;
+
+  // console.log(name, cityId, campusId);
+
+  if (!name) {
+    throw new apiError(400, "Course name is required");
+  }
+  if (!cityId) {
+    throw new apiError(400, "City is required");
+  }
+  if (!campusId) {
+    throw new apiError(400, "Campus is required");
+  }
+
+  const user = await Admin.findById(req.admin._id);
+  const city = await City.findById(cityId);
+  const campus = await Campus.findById(campusId);
+
+  if (!city) {
+    throw new apiError(404, "City not found");
+  }
+  if (!campus) {
+    throw new apiError(404, "Campus not found");
+  }
+  if (!user) {
+    throw new apiError(404, "Unauthorized user");
+  }
+
+  const existedCourse = await Course.findOne({ name });
+
+  if (existedCourse) {
+    const existedCity =
+      Array.isArray(existedCourse.city) && existedCourse.city.includes(cityId);
+    const existedCampus =
+      Array.isArray(existedCourse.campus) &&
+      existedCourse.campus.includes(campusId);
+
+    if (existedCity && existedCampus) {
+      throw new apiError(400, "Course already exists");
     }
-  
-    const allCourses = await Course.find()
+
+    if (existedCity && !existedCampus) {
+      existedCourse.campus.push(campusId);
+      await existedCourse.save();
+    }
+
+    if (!existedCity) {
+      existedCourse.city.push(cityId);
+      existedCourse.campus.push(campusId);
+      await existedCourse.save();
+    }
+
+      const course = await Course.findById(existedCourse._id)
+        .populate("city", "cityName")
+        .populate("campus", "name")
+        .populate({
+          path: "createdBy",
+          select: "fullName email phoneNumber gender",
+          populate: [
+            { path: "city", select: "cityName" },
+            { path: "campus", select: "name" },
+          ],
+        });
+      return res
+        .status(201)
+        .json(new apiResponse(201, course, "Course added successfully"));
+  } else {
+    const newCourse = new Course({
+      name,
+      city: [cityId],
+      campus: [campusId],
+      createdBy: req.admin._id,
+    });
+
+    await newCourse.save();
+    const course = await Course.findById(newCourse._id)
       .populate("city", "cityName")
       .populate("campus", "name")
       .populate({
@@ -183,10 +211,12 @@ const addCourse = asyncHandler(async (req, res) => {
           { path: "campus", select: "name" },
         ],
       });
-  
-    res.status(201).json(new apiResponse(201, allCourses, "Course added and all courses fetched successfully"));
-  });
-  
+
+    return res
+      .status(201)
+      .json(new apiResponse(201, course, "Course added successfully"));
+  }
+});
 
 const getCourse = asyncHandler(async (req, res) => {
   const courses = await Course.find();
@@ -226,4 +256,208 @@ const getAllCourses = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, courses, "Courses fetched successfully"));
 });
 
-export { addCourse, getCourse, getAllCourses };
+const editCourse = asyncHandler(async (req, res) => {
+  const { courseId } = req.params;
+  const { courseName } = req.body;
+
+  console.log(courseId, courseName);
+
+  if (!courseName) throw new apiError(400, "Course name is required");
+
+  const course = await Course.findByIdAndUpdate(
+    {
+      _id: courseId,
+    },
+    {
+      name: courseName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("city", "cityName")
+    .populate("campus", "name")
+    .populate({
+      path: "createdBy",
+      select: "fullName email phoneNumber gender",
+      populate: [
+        { path: "city", select: "cityName" },
+        { path: "campus", select: "name" },
+      ],
+    });
+
+  console.log(course);
+
+  res
+    .status(200)
+    .json(new apiResponse(200, course, "Course updated successfully"));
+});
+
+const deleteCourse = asyncHandler(async (req, res) => {
+  const { courseId } = req.params;
+  const course = await Course.findByIdAndDelete(courseId);
+  res
+    .status(200)
+    .json(new apiResponse(200, null, "Course deleted successfully"));
+});
+
+const deleteCourseCampus = asyncHandler(async (req, res) => {
+  const { courseId, campusId } = req.params;
+
+  console.log(courseId, campusId);
+  
+
+  const existedCourse = await Course.findById(courseId);
+  const existedCampus = await Campus.findById(campusId);
+
+
+  if (!existedCourse || !existedCampus) {
+    return res.status(404).json({ message: "Course or Campus not found" });
+  }
+
+  const campusesInCity = await Campus.find({ city: existedCampus.city });
+
+  const campusesInCourseOfSameCity = campusesInCity.filter((campus) =>
+    existedCourse.campus.includes(campus._id.toString())
+  );
+
+  if (campusesInCourseOfSameCity.length === 1) {
+    existedCourse.campus = existedCourse.campus.filter(
+      (id) => id.toString() !== campusId
+    );
+    existedCourse.city = existedCourse.city.filter(
+      (id) => id.toString() !== existedCampus.city.toString()
+    );
+
+    await existedCourse.save();
+
+    const course = await Course.findById(existedCourse._id)
+      .populate("city", "cityName")
+      .populate("campus", "name")
+      .populate({
+        path: "createdBy",
+        select: "fullName email phoneNumber gender",
+        populate: [
+          { path: "city", select: "cityName" },
+          { path: "campus", select: "name" },
+        ],
+      })
+
+      console.log(course);
+
+    res
+      .status(200)
+      .json(
+        new apiResponse(
+          200,
+          course,
+          "Campus deleted successfully"
+        )
+      );
+  } else if (campusesInCourseOfSameCity.length > 1) {
+    existedCourse.campus = existedCourse.campus.filter(
+      (id) => id.toString() !== campusId
+    );
+
+    await existedCourse.save();
+
+    const course = await Course.findById(existedCourse._id)
+      .populate("city", "cityName")
+      .populate("campus", "name")
+      .populate({
+        path: "createdBy",
+        select: "fullName email phoneNumber gender",
+        populate: [
+          { path: "city", select: "cityName" },
+          { path: "campus", select: "name" },
+        ],
+      })
+
+      console.log(course);
+      
+    res
+      .status(200)
+      .json(
+        new apiResponse(200, course, "Campus deleted successfully")
+      );
+  }
+});
+
+const deleteCourseCity = asyncHandler(async (req, res) => {
+  const { courseId, cityId } = req.params;
+
+  // console.log(courseId, cityId);
+  
+  if (!cityId) {
+    throw new apiError(404, "City not found");
+  }
+
+  if (!courseId) {
+    throw new apiError(404, "City not found");
+  }
+
+  const existedCourse = await Course.findById(courseId);
+  const campusesOfCurrentCity = await Campus.find({ city: cityId });
+
+  // console.log(existedCourse, campusesOfCurrentCity);
+  
+
+  if (!existedCourse) {
+    throw new apiError(404, "Course not found");
+  }
+
+  if(!campusesOfCurrentCity){
+    throw new apiError(404, "Campuses not found");
+  }
+ 
+
+  const campusIdsOfCurrentCity = campusesOfCurrentCity.map((campus) => campus._id);
+
+  // console.log("Course Available in Campuses: ",existedCourse.campus);
+  // console.log("Campus Ids of Current City: ",campusIdsOfCurrentCity);
+
+  const campusToBeDeleted = existedCourse.campus.filter( id => campusIdsOfCurrentCity.toString().includes(id.toString()) );
+
+  // console.log("Campus to be deleted: ",campusToBeDeleted);
+
+  if(!campusToBeDeleted){
+    throw new apiError(404, "Campus not found in course");
+  }
+
+  existedCourse.campus = existedCourse.campus.filter(
+    (id) => !campusToBeDeleted.toString().includes(id.toString())
+  );
+
+  existedCourse.city = existedCourse.city.filter(
+    (id) => id.toString() !== cityId
+  );
+
+  await existedCourse.save();
+
+  const course = await Course.findById(existedCourse._id)
+      .populate("city", "cityName")
+      .populate("campus", "name")
+      .populate({
+        path: "createdBy",
+        select: "fullName email phoneNumber gender",
+        populate: [
+          { path: "city", select: "cityName" },
+          { path: "campus", select: "name" },
+        ],
+      })
+      console.log(course);
+
+  res
+    .status(200)
+    .json(new apiResponse(200, course, "City deleted successfully"));
+});
+
+export {
+  addCourse,
+  getCourse,
+  getAllCourses,
+  editCourse,
+  deleteCourse,
+  deleteCourseCampus,
+  deleteCourseCity,
+};
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,72 +1,76 @@
-import { Router } from 'express'
+import { Router } from "express";
 import {
-    registerAdmin,
-    loginAdmin,
-    logoutAdmin,
-    getCurrentAdmin,
-    refreshAdminAccessToken,
-    updateProfilePicture,
+  registerAdmin,
+  loginAdmin,
+  logoutAdmin,
+  getCurrentAdmin,
+  refreshAdminAccessToken,
+  updateProfilePicture,
   updateProfileDetails,
   getAllAdmins,
-} from '../controllers/admin.controllers.js'
+} from "../controllers/admin.controllers.js";
+import { getCity, addCity } from "../controllers/city.controller.js";
+import { addCampus, getCampus } from "../controllers/campus.controllers.js";
 import {
-    getCity, 
-    addCity,
-} from '../controllers/city.controller.js'
+  addCourse,
+  getCourse,
+  getAllCourses,
+  editCourse,
+  deleteCourse,
+  deleteCourseCampus,
+  deleteCourseCity,
+} from "../controllers/course.controllers.js";
 import {
-    addCampus,
-    getCampus,
-} from '../controllers/campus.controllers.js'
-import {
-    addCourse,
-    getCourse,
-    getAllCourses,
-} from '../controllers/course.controllers.js'
-import {
-    addClass,
-    // getClass,
-    getAllClasses,
-    getTeachersByCourse
-} from '../controllers/class.controllers.js'
-import {
-    getAllTeachers,
-} from '../controllers/teacher.controllers.js'
-import { upload } from '../middlewares/multer.middleware.js'
-import { verifyAdminJWT } from '../middlewares/admin.auth.middleware.js'
+  addClass,
+  // getClass,
+  getAllClasses,
+  getTeachersByCourse,
+} from "../controllers/class.controllers.js";
+import { getAllTeachers } from "../controllers/teacher.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyAdminJWT } from "../middlewares/admin.auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 // routes
 
-router.route('/register').post(upload.single('profile'), registerAdmin)
-router.route('/login').post(loginAdmin)
+router.route("/register").post(upload.single("profile"), registerAdmin);
+router.route("/login").post(loginAdmin);
 
 // secure routes
 
-router.route('/logout').post(verifyAdminJWT, logoutAdmin)
-router.route('/getCurrentAdmin').get(verifyAdminJWT, getCurrentAdmin)
-router.route('/refreshAdminAccessToken').post(refreshAdminAccessToken)
+router.route("/logout").post(verifyAdminJWT, logoutAdmin);
+router.route("/getCurrentAdmin").get(verifyAdminJWT, getCurrentAdmin);
+router.route("/refreshAdminAccessToken").post(refreshAdminAccessToken);
 
-router.route('/updateAdminProfilePicture').put(verifyAdminJWT, upload.single('profile'), updateProfilePicture)
-router.route('/updateAdminProfileDetails').put(verifyAdminJWT, updateProfileDetails)
+router
+  .route("/updateAdminProfilePicture")
+  .put(verifyAdminJWT, upload.single("profile"), updateProfilePicture);
+router
+  .route("/updateAdminProfileDetails")
+  .put(verifyAdminJWT, updateProfileDetails);
 
-router.route('/addCity').post(verifyAdminJWT, addCity)
-router.route('/getCities').get(verifyAdminJWT, getCity)
+router.route("/addCity").post(verifyAdminJWT, addCity);
+router.route("/getCities").get(verifyAdminJWT, getCity);
 
-router.route('/addCampus').post(verifyAdminJWT, addCampus)
-router.route('/getCampuses').get(verifyAdminJWT, getCampus)
+router.route("/addCampus").post(verifyAdminJWT, addCampus);
+router.route("/getCampuses").get(verifyAdminJWT, getCampus);
 
-router.route('/addCourse').post(verifyAdminJWT, addCourse)
-router.route('/getCourses').get(verifyAdminJWT, getCourse)
-router.route('/getAllCourses').get(verifyAdminJWT, getAllCourses)
+router.route("/addCourse").post(verifyAdminJWT, addCourse);
+router.route("/getCourses").get(verifyAdminJWT, getCourse);
+router.route("/getAllCourses").get(verifyAdminJWT, getAllCourses);
+router.route("/editCourse/:courseId").put(verifyAdminJWT, editCourse);
+router.route("/deleteCourse/:courseId").delete(verifyAdminJWT, deleteCourse);
 
-router.route('/addClass').post(verifyAdminJWT, addClass)
+router.route("/addClass").post(verifyAdminJWT, addClass);
 // router.route('/getClass').get(verifyAdminJWT, getClass)
 // router.route('/getClasses').get(verifyAdminJWT, getClasses)
-router.route('/getTeachersByCourse').get(verifyAdminJWT, getTeachersByCourse)
-router.route('/getAllTeachers').get(verifyAdminJWT, getAllTeachers)
-router.route('/getAllAdmins').get(verifyAdminJWT, getAllAdmins)
-router.route('/getAllClasses').get(verifyAdminJWT, getAllClasses)
+router.route("/getTeachersByCourse").get(verifyAdminJWT, getTeachersByCourse);
+router.route("/getAllTeachers").get(verifyAdminJWT, getAllTeachers);
+router.route("/getAllAdmins").get(verifyAdminJWT, getAllAdmins);
+router.route("/getAllClasses").get(verifyAdminJWT, getAllClasses);
 
+router.route("/deleteCourseCity/:cityId&:courseId").delete(verifyAdminJWT, deleteCourseCity);
+router.route("/deleteCourseCampus/:campusId&:courseId").delete(verifyAdminJWT,deleteCourseCampus);
 
-export default router
+export default router;
