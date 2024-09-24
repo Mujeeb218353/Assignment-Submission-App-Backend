6cce845ac7ffd30e7974ecd0d879acd680c5f83b
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-09-24 23:24:52 +0500

    Fixed Multer Issue

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    const dir = path.resolve("public/temp");
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
